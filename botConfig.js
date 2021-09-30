5f6f7a35fe7a62b6d4b539e944060952bfc3e23c
Caramel <91426528+CaramelBun@users.noreply.github.com>
2021-09-30 15:15:37 +0800

    Update botConfig.js

--- botConfig.js
+++ botConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-    token: "ODkwMjE4NDM4ODEzOTU4MTc0.YUsmhw.4YQhVr38OLOZAq9lUB4m6yJ-1ao",
+    token: "",
     prefix: "s!",
     admins: [
-        '823068643311091732'
+        ''
 ],
     debug: true,
     countChannel: "countChannelID"
