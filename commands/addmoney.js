2ec61a5d02c1e2c2ef97f3becccc7884ad1b6bd2
Caramel <91426528+CaramelBun@users.noreply.github.com>
2021-09-30 17:29:56 +0800

    Update addmoney.js

--- commands/addmoney.js
+++ commands/addmoney.js
@@ -1,12 +1,19 @@
 const { MessageEmbed } = require("discord.js");
-
+const db = require("quick.db");
+let userInfo = db.get('userInfo')
+if(userInfo == " " || "undefined") {
+    db.set( `${message.author.id`, { userID : `${message.author.id}`})
+    db.push("`${message.author.id`}.balance", "0")
+} else {
+return;
+}
 exports.execute = async (client, message, args) => {
     if (!client.config.admins.includes(message.author.id)) return message.reply('You are not permitted to use this command!');
     let user = message.mentions.users.first();
     if (!user) return message.channel.send("Please specify a user!");
     let amount = args[1];
     if (!amount || isNaN(amount)) return message.reply("Please specify a valid amount.");
-    let data = client.eco.addMoney(user.id, parseInt(amount));
+    let data = db.add(`${user.id}.balance`, parseInt(amount));
     const embed = new MessageEmbed()
         .setTitle(`Money Added!`)
         .addField(`User`, `<@${data.user}>`)
